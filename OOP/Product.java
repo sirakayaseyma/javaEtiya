1fdbfec0f063a845411f9f01564591215fbe1fd3
Şeyma <sirakaya.seymaa@gmail.com>
2024-04-25 15:30:55 +0300

    oop 25.04.2023

--- OOP/Product.java
+++ OOP/Product.java
@@ -3,15 +3,46 @@ package OOP;
 public class Product {
     //Alan tanımlarken camelCase yazıyoruz.
     
-    public int id;
+    private int id;
     
-    public String name;
+    private String name;
 
-    public double unitPrice;
+    private double unitPrice;
 
-    public int unitInStock;
+    private int unitInStock;
+
+    //encapsulation 
+
+    public int getId(){ // burada id değerimi get edebilirim
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public double getUnitPrice(){
+        return this.unitPrice;
+    }
+    public void setUnitPrice(double unitPrice){
+        this.unitPrice = unitPrice;
+    }
 
 
+    public int getUnitInStock(){
+        return this.unitInStock;
+    }
+    public void setUnitInStock(int unitInStock){
+        this.unitInStock = unitInStock;
+    }
     public Product(int id,String name ,double unitPrice , int unitInStock){
         this.id = id;
         this.name = name;
@@ -20,6 +51,6 @@ public class Product {
     }
 
     public Product(){
-        
+
     }
 }
--- OOP/ProductManager.java
+++ OOP/ProductManager.java
@@ -3,32 +3,74 @@ package OOP;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Iterator;
+
+
 public class ProductManager {
     
-    private List<Product> products = new ArrayList<>();
+    private List<Product> products;
+
+    public ProductManager(){
+        products = new ArrayList<Product>();
+        products.add(new Product(1,"Laptop", 25000, 2));
+        products.add(new Product(2,"Tablet",1200, 5));
+
+    }
 
     public  List<Product> getAll(){
 
-
-        Product product1 = new Product(1,"Laptop", 25000, 2);
-        
-        Product product2 = new Product(2,"Tablet",1200, 5);
-        
-        Product product3 = new Product(); 
-       
-        products.add(product1);
-        products.add(product2);
-    
         return products;
 
     }
 
     public  void addBasketProduct(Product product){
-        System.out.println("Basariyla sepete eklendi " + product.name);
+        System.out.println("Basariyla sepete eklendi " + product.getName());
     }
 
     public void add(Product product) {
          products.add(product);
     }
     
+    public Product getById(int id){
+        for(Product product: products){
+            if(product.getId()==id){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product getByName(String productName){
+        for(Product product: products){
+            if(product.getName()==productName){
+                return product;
+            }
+    }
+    return null; 
+}
+
+    public void update(Product product){
+        for(Product updateProduct: products){
+            if(updateProduct.getId()==product.getId()){
+                updateProduct.setName(product.getName());
+                updateProduct.setUnitPrice(product.getUnitPrice());
+            }
+        }
+    }
+
+   
+   
+    public void delete(int id){
+        
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if(product.getId() == id){
+                iterator.remove();
+            }
+        }
+        
+    
+    }
+
 }
