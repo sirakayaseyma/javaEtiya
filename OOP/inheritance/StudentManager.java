fc0920e01da1cedbda5dd6cc11440d667cd685bf
Şeyma <sirakaya.seymaa@gmail.com>
2024-04-26 16:09:39 +0300

    polymorfizm -  log file and database

--- OOP/inheritance/StudentManager.java
+++ OOP/inheritance/StudentManager.java
@@ -4,10 +4,19 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import OOP.polymorfizm.DatabaseLogger;
+import OOP.polymorfizm.FileLogger;
+import OOP.polymorfizm.Logger;
+
 public class StudentManager {
     private List<Student> students;
 
+    private Logger logger;
+
     public StudentManager(){
+        //logger = new DatabaseLogger();
+        // İki loggerdan birini almam gerekir. İkisinin sonuç vermesi isteniyorsa bir array içine atama yapmam gerekir.
+        logger = new FileLogger();
         students = new ArrayList<>();
        Student student = new Student();
        student.setId(1);
@@ -21,10 +30,14 @@ public class StudentManager {
     }
 
     public List<Student> getAll(){
+        for(Student student:students){
+            logger.log(student.getFirstName() + " " + student.getLastName());
+        }
         return students;
     }
 
     public void add(Student student){
+        logger.log(student.getFirstName() +" " + student.getLastName());
         student.setCreatedDate(LocalDateTime.now());
         students.add(student);
     }
